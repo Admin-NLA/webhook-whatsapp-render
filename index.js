1f02ecadf8be0d2f4134c5522393d197c8acbf84
Admin-NLA <sistemas.gruponoria@gmail.com>
2025-07-10 14:21:46 -0600

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 const VERIFY_TOKEN = 'zoho2025'; // El token que configuras en Meta para validar
-const ZOHO_FUNCTION_URL = 'https://www.zohoapis.com/crm/v7/functions/webhook_whatsapp_handler_1/actions/execute?auth_type=apikey&zapikey=1003.93312cde5a12a3d6728fb8426b6500e9.cb429661b7b898284c756e05c50e4f4e';
+const ZOHO_FUNCTION_URL = 'https://www.zohoapis.com/crm/v7/functions/webhook_whatsapp_handler_1/actions/execute?auth_type=apikey&zapikey=1003.fc864949ec8b86d4d7f29d06b59645ca.56181fa66c0726afcc57ccfaecf6c592';
 
 // Validar webhook con Meta (GET)
 app.get('/webhook', (req, res) => {
