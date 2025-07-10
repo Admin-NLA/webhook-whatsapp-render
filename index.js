c92c6f1c35254922e8f55983557c864caf29c7ad
Admin-NLA <sistemas.gruponoria@gmail.com>
2025-07-10 13:53:45 -0600

    index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Servidor escuchando en puerto ${PORT}`);
 });
